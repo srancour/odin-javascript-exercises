89db255ec2845ce0ead3628302150f5beb9b6137
Steve Rancour <18683379+srancour@users.noreply.github.com>
2025-12-01 15:10:58 -0500

    feat: add function to split, reverse, and join a string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
-
+const reverseString = function(string) { // Take in a string
+    // First attempt
+    // // Split string into an array for every character
+    // arr = string.split('');
+    // // Create new array for the old to go into
+    // let newarr = [];
+    // // Reverse order of array
+    // for (let i = 0; i <= arr.length; i++) {
+    //     newarr[(arr.length - i)] = arr[i];
+    // }
+    // reversedString = newarr.join('');
+    // return reversedString;
+    // // Combine the array back into a string
+    
+    // Cleaner approach
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
