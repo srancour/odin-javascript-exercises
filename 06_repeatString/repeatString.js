fc9a0773efde9e86dc7eab49b17d98fb738da24f
Steve Rancour <18683379+srancour@users.noreply.github.com>
2025-12-01 14:35:57 -0500

    feat: add function to repeat a string and allow all tests

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) { // Take two arguments string and num
+    let result = "";
+    if (num < 0) { // If num is < 0 return ERROR
+        result = "ERROR"
+    } else {
+        for (let i = 0; i < num; i++){ // Use a loop to repeat
+            result += string; // Returns string num times
+        }
+    }
 
+    return result;
 };
 
 // Do not edit below this line
