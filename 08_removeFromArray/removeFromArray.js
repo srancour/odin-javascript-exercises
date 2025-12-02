f732aa9383aad54066e098c9fe1c0411f1de2e91
Steve Rancour <18683379+srancour@users.noreply.github.com>
2025-12-02 15:03:56 -0500

    feat: add simpler way of doing the same

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,8 +1,13 @@
 const removeFromArray = function(arr, ...removals) { // Function that takes in an array and as many optional removals as it wants
-    for (let i = 0; i <= removals.length; i++) { //iterate through all elements passed to removals
-        arr = arr.filter(value => value !== removals[i]); // removes individual elements where they equal the current optional removal option
-    }
-    return arr;
+    // First attempt
+    // for (let i = 0; i <= removals.length; i++) { //iterate through all elements passed to removals
+    //     arr = arr.filter(value => value !== removals[i]); // removes individual elements where they equal the current optional removal option
+    // }
+    // return arr;
+
+    // Better way of doing it
+    return arr.filter(val => !removals.includes(val));
+
 };
 
 // console.log(removeFromArray([1, 2, 3, 4], 3, 2));
