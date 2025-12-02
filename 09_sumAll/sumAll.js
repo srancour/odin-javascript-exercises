7e47311839a72fc2def889786538a607e3a3abc7
Steve Rancour <18683379+srancour@users.noreply.github.com>
2025-12-02 15:03:10 -0500

    feat: sum all values checking for correct variable types

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) { // Take in 2 positive variables and return sum of every integer between inclusively
+    let sum = 0;
+    if (Number.isInteger(a) && Number.isInteger(b) && a >= 0 && b >= 0){ // checks if both are integers and positive
+        if (a > b){ // allows for either value to be larger
+            for (let i = b; i <= a; i++) {
+                sum += i;
+            }
+        } else {
+            for (let i = a; i <= b; i++) {
+                sum += i;
+            }
+        }
+        return sum;
+    } else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
