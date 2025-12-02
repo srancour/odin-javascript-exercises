c41c372ee40dec26bd8f0e53d76cb0b4ffe80526
Steve Rancour <18683379+srancour@users.noreply.github.com>
2025-12-02 14:34:52 -0500

    feat: add function to remove elements from an array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removals) { // Function that takes in an array and as many optional removals as it wants
+    for (let i = 0; i <= removals.length; i++) { //iterate through all elements passed to removals
+        arr = arr.filter(value => value !== removals[i]); // removes individual elements where they equal the current optional removal option
+    }
+    return arr;
 };
+
+// console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
