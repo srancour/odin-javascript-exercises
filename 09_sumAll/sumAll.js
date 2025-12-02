bceae44190defdab4085cfd439077e875b315d70
Steve Rancour <18683379+srancour@users.noreply.github.com>
2025-12-02 17:04:03 -0500

    feat: add alt way to deal with different higher value

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,14 +1,19 @@
 const sumAll = function(a, b) { // Take in 2 positive variables and return sum of every integer between inclusively
     let sum = 0;
     if (Number.isInteger(a) && Number.isInteger(b) && a >= 0 && b >= 0){ // checks if both are integers and positive
-        if (a > b){ // allows for either value to be larger
-            for (let i = b; i <= a; i++) {
-                sum += i;
-            }
-        } else {
-            for (let i = a; i <= b; i++) {
-                sum += i;
-            }
+        // Other way to do the same
+        if (a > b) [a, b] = [b, a];//{ // allows for either value to be larger
+        //     // First attempt
+        //     for (let i = b; i <= a; i++) {
+        //         sum += i;
+        //     }
+        // } else {
+        //     for (let i = a; i <= b; i++) {
+        //         sum += i;
+        //     }
+        //}
+        for (let i = a; i <= b; i++) {
+            sum += i;
         }
         return sum;
     } else {
