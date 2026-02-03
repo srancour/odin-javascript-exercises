801b2ca21eecf919186a102e7318f0cc405e4d58
Steve Rancour <18683379+srancour@users.noreply.github.com>
2026-02-03 17:34:39 -0500

    feat: add function for fibonacci checker

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (typeof number == "number") {
 
+    } else {
+        number = Number(number);
+    }
+    let value = 1;
+    let previous = 0;
+    if (number < 0){
+        return "OOPS";
+    } else if (number == 0){
+        return 0;
+    } else {
+        for (let i = 2; i <= number; i++) {
+            let current = value + previous;
+            previous = value;
+            value = current;
+        }
+        return value;
+    }
+    
 };
 
 // Do not edit below this line
