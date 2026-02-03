3a38347e14301f196db477de8a2bdda5df95185c
Steve Rancour <18683379+srancour@users.noreply.github.com>
2026-02-03 18:08:26 -0500

    feat: add function to find the oldest person

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+function getAge(person) {
+    person.yearOfDeath ??= ((new Date()).getFullYear());
+    return person.yearOfDeath - person.yearOfBirth;
+}
+const findTheOldest = function(people) {
+    const oldestSort = people.sort((a, b) => {
+        return getAge(b) - getAge(a);
+    });
+    return oldestSort[0];
 };
 
 // Do not edit below this line
