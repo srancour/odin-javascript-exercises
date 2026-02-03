68436c505a31165901ac8601079667a97874ed5f
Steve Rancour <18683379+srancour@users.noreply.github.com>
2026-02-03 11:44:43 -0500

    feat: add functions for calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let result = 1;
+  if (number === 0 || number === 1) {
+
+  } else if (number < 0) {
+    return "Factorial is not defined for negative numbers";
+  } else {
+    for (let i = 2; i <= number; i++) {
+      result *= i;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
